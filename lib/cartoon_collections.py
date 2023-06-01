53e620c0253974c224ebb07f30d3e3430cd1a13a
Mark Nosal <mark.c.nosal@gmail.com>
2023-06-01 17:34:15 -0500

    finished lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,18 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarves):
+    [print(f"{index+1}. {dwarves[index]}") for index in range(len(dwarves))]
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [call.capitalize() + '!' for call in planeteer_calls]
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(planeteer_calls):
+    for call in planeteer_calls:
+        if len(call) > 4:
+            return True
+    return False
 
-def find_the_cheese():
-    pass
+def find_the_cheese(snacks):
+    cheese_types = ['cheddar', 'gouda', 'camembert']
+    for snack in snacks:
+        if snack in cheese_types:
+            return snack
+    return None
